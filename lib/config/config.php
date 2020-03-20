824f0f41cf28f99318fa18a67e9502f120532624
TanHongIT <phuongtan12357nguyen@gmail.com>
2020-03-20 17:33:35 +0700

    rename project

--- lib/config/config.php
+++ lib/config/config.php
@@ -1,5 +1,5 @@
 <?php
-define('BASE_URL', 'generalshop');
-define('PATH_URL', 'http://localhost/generalshop/');
+define('BASE_URL', 'php-mvc-shop');
+define('PATH_URL', 'http://localhost/php-mvc-shop/');
 define('PATH_URL_IMG', PATH_URL . '/public/upload/images/');
 define('PATH_URL_IMG_PRODUCT', PATH_URL . '/public/upload/products/');

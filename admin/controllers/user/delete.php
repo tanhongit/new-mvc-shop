c4a19df0250527eb3caf98a5f07185471b4eec7a
TanHongIT <phuongtan12357nguyen@gmail.com>
2020-04-08 21:49:47 +0700

    fix delete users

--- admin/controllers/user/delete.php
+++ admin/controllers/user/delete.php
@@ -2,5 +2,13 @@
 permission_user();
 require_once('admin/models/users.php');
 $user_id = intval($_GET['user_id']);
-user_delete($user_id);
+global $user_nav;
+$user = get_a_record('users', $user_nav);
+if ($_GET['user_id'] == $user_nav || $user['role_id'] <> 1) {
+    echo '<div style="padding-top: 200" class="container"><div class="alert alert-danger" style="text-align: center;"><strong>Error!</strong> Bạn không có quyền hoặc không được phép xóa người dùng này.<br><br> Hãy <a href="javascript: history.go(-1)">Quay lại</a> .!!</div></div>';
+    require('admin/views/user/result.php');
+    exit;
+} else {
+    user_delete($user_id);
+}
 header('location:admin.php?controller=user&action=listall');
--- lib/functions.php
+++ lib/functions.php
@@ -80,10 +80,30 @@ function stringURL($str)
     $str = trim($str);
     return $str;
 }
+function convert_name($str)
+{
+    $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
+    $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
+    $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
+    $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $str);
+    $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
+    $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
+    $str = preg_replace("/(đ)/", 'd', $str);
+    $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
+    $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
+    $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
+    $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $str);
+    $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $str);
+    $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $str);
+    $str = preg_replace("/(Đ)/", 'D', $str);
+    $str = preg_replace("/(\“|\”|\‘|\’|\,|\!|\&|\;|\@|\#|\%|\~|\`|\=|\_|\'|\]|\[|\}|\{|\)|\(|\+|\^)/", '-', $str);
+    $str = preg_replace("/( )/", '-', $str);
+    return $str;
+}
 function slug($str)
 {
     $str = stringURL($str);
-    $str = strtolower($str);
+    $str = strtolower($str); //mb_strtolower($str, 'UTF-8');
     $str = str_replace(' ', '-', $str);
     return $str;
 }

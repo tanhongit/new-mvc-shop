117d2a16a0259f990879731aff24a253fb277150
TanHongIT <phuongtan12357nguyen@gmail.com>
2020-04-09 11:04:17 +0700

    Update model.php
    add get time

--- lib/model.php
+++ lib/model.php
@@ -79,3 +79,33 @@ function select_a_record($table, $options = array(), $select = '*')
     }
     return $data;
 }
+function get_time($timePost,$timeReply) {
+
+    $datePost	= date_parse_from_format('Y:m:d H:i:s', $timePost);
+    $dateReply	= date_parse_from_format('Y:m:d H:i:s', $timeReply);
+
+    $tsPost		= mktime($datePost['hour'], $datePost['minute'], $datePost['second'], $datePost['month'], $datePost['day'], $datePost['year']);
+    $tsReply	= mktime($dateReply['hour'], $dateReply['minute'], $dateReply['second'], $dateReply['month'], $dateReply['day'], $dateReply['year']);
+    $distance 	= $tsReply - $tsPost;
+
+    switch ($distance){
+        case ($distance < 60):
+            $result = ($distance == 1) ? $distance . ' second ago' : $distance . ' seconds ago';
+            break;
+        case ($distance >= 60 && $distance < 3600):
+            $minute	= round($distance/60);
+            $result = ($minute == 1) ? $minute . ' minute ago' : $minute . ' minutes ago';
+            break;
+        case ($distance >= 3600 && $distance < 86400):
+            $hour	= round($distance/3600);
+            $result = ($hour == 1) ? $hour . ' hour ago' : $hour . ' hours ago';
+            break;
+        case (round($distance/86400)==1):
+            $result = 'Yesterday at ' . date('H:i:s', $tsReply);
+            break;
+        default:
+            $result = date('d/m/Y \a\t H:i:s', $tsPost);
+            break;
+    }
+    return $result;
+}

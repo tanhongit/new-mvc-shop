811224357d1276ed5217f46c22ac66c52cd3082c
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-07-15 22:52:46 +0700

    fix comment - umage user comment

--- content/controllers/comment/index.php
+++ content/controllers/comment/index.php
@@ -5,16 +5,29 @@
 -->
 <?php
 if (!empty($_POST)) {
-    $comment_add = array(
-        'id' => 0,
-        'product_id' => intval($_POST['product_id']),
-        'email' => escape($_POST['email']),
-        'author' => escape($_POST['author']),
-        'content' => escape($_POST['content']),
-        'createDate' => gmdate('Y-m-d H:i:s', time() + 7 * 3600),
-        'user_id' => intval($_POST['user_id']),
-        'link_image' => escape($_POST['link_image'])
-    );
+    global $user_nav;
+    if (isset($user_nav)) {
+        $comment_add = array(
+            'id' => 0,
+            'product_id' => intval($_POST['product_id']),
+            'email' => escape($_POST['email']),
+            'author' => escape($_POST['author']),
+            'content' => escape($_POST['content']),
+            'createDate' => gmdate('Y-m-d H:i:s', time() + 7 * 3600),
+            'user_id' => intval($_POST['user_id']),
+            'link_image' => escape($_POST['link_image'])
+        );
+    } else {
+        $comment_add = array(
+            'id' => 0,
+            'product_id' => intval($_POST['product_id']),
+            'email' => escape($_POST['email']),
+            'author' => escape($_POST['author']),
+            'content' => escape($_POST['content']),
+            'createDate' => gmdate('Y-m-d H:i:s', time() + 7 * 3600),
+            'user_id' => intval($_POST['user_id'])
+        );
+    }
     save('comments', $comment_add);
     echo "<div class='alert alert-success'><strong>Done!</strong> Bạn đã ghi lời bình luận của bạn lại thành công !! <br>Hãy <a href='javascript: history.go(-1)'>Trở lại sản phẩm</a> hoặc <a href='index.php'>Đến trang chủ</a></div>";
 }

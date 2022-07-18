2a1de6c5d517663e99f8a1df27d8b2bb9d3365ac
tanhongit <phuongtan12357nguyen@gmail.com>
2022-07-18 10:29:32 +0700

    fix get controller and action for homepage

--- index.php
+++ index.php
@@ -24,10 +24,18 @@ require "lib/counter.php";
 //     }
 // }
 // counting_ip();
-if (isset($_GET['controller'])) $controller = $_GET['controller'];
-else $controller = 'home';
-if (isset($_GET['action'])) $action = $_GET['action'];
-else $action = 'index';
+if (isset($_GET['controller']) && '' != $_GET['controller']) {
+    $controller = $_GET['controller'];
+} else {
+    $controller = 'home';
+}
+
+if (isset($_GET['action']) && '' != $_GET['action']) {
+    $action = $_GET['action'];
+} else {
+    $action = 'index';
+}
+
 $file = 'content/controllers/' . $controller . '/' . $action . '.php';
 if (file_exists($file)) {
     require($file);
